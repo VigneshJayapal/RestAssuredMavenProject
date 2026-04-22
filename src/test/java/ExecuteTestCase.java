be4f6255a08e511d0ece9d1c5cccab73bc08fc33
Vignesh Jayapal <vigneshjayapal7@gmail.com>
2026-04-23 01:02:35 +0530

    TestNg xml config

--- src/test/java/ExecuteTestCase.java
+++ src/test/java/ExecuteTestCase.java
@@ -1,19 +1,26 @@
+import io.qameta.allure.*;
 import io.qameta.allure.testng.AllureTestNg;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners({AllureTestNg.class})
 public class ExecuteTestCase {
 
-    public static UserService userService = new UserService();
-    @Test
-    public static void getMethodTest(){
+    UserService userService = new UserService();
+
+    @Test(description = "Validate GET API")
+    @Severity(SeverityLevel.CRITICAL)
+    public void getMethodTest() {
+
         Response response = userService.getRequest();
+
         int statusCode = response.getStatusCode();
-        System.out.println("Status Code is:"+statusCode);
-        ResponseBody responseBody = response.getBody();
-        System.out.println(responseBody.prettyPrint());
+        System.out.println("Status Code: " + statusCode);
+
+        Assert.assertEquals(statusCode, 200);
+
+        System.out.println(response.getBody().prettyPrint());
     }
 }
